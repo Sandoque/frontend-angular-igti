a80147548a83ec249d474cfe8aca0bda5acfa3fa
Sandoque <acsandoque@gmail.com>
2020-10-04 17:33:28 -0300

    data binding básico

--- minha-app/src/app/app.module.ts
+++ minha-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { C1Component } from './c1.component';
 import { C2Component } from './c2/c2.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent, C1Component, C2Component],
+  declarations: [AppComponent, C1Component, C2Component, CounterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
