f7567e390c7522a79c23f50153771ad5b1ccd9fc
Sandoque <acsandoque@gmail.com>
2020-10-04 16:57:14 -0300

    Introdução aos Componentes e templates

---
+++ minha-app/src/app/c1.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'c1',
+  templateUrl: './c1.component.html',
+})
+export class C1Component {}
---
+++ minha-app/src/app/c2/c2.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c2',
+  templateUrl: './c2.component.html',
+  //template: 'Este é o meu template', //Template inline passado diretamente
+  styleUrls: ['./c2.component.css'],
+})
+export class C2Component {}
