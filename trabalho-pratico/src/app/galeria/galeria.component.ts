b2903c15fffcf38796eb8a9701fde4b368485211
Sandoque <acsandoque@gmail.com>
2020-10-07 06:19:52 -0300

    atualizando trabalho pratico

---
+++ trabalho-pratico/src/app/galeria/galeria.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css'],
+})
+export class GaleriaComponent {
+  @Input() titulo: string = '';
+  @Input() fotos: string[] = [];
+  fotoAtual: number = 0;
+
+  mudaParaProxima() {
+    this.fotoAtual++;
+  }
+
+  voltaParaAnterior() {
+    this.fotoAtual--;
+  }
+
+  estaNaUltimaFoto() {
+    this.fotoAtual == this.fotos.length - 1;
+  }
+}
